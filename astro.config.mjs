afd259880321ede76d2db5cec7726e19df409f43
achtaitaipai <charles.cailleteau@gmail.com>
2024-03-24 19:24:39 +0100

    feat site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://odyc-site.pages.dev/",
   integrations: [svelte(), tailwind()],
   markdown: {
     shikiConfig: {
