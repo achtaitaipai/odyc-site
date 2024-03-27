c5a0ceab2cd7b59403a8b2847144c86324d024b5
achtaitaipai <charles.cailleteau@gmail.com>
2024-03-27 10:04:32 +0100

    black and white

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
-<button on:click on:keydown class="bg-blue-400 text-white px-2.5 py-1 rounded">
+<button
+  on:click
+  on:keydown
+  class="border border-gray-300 text-black px-2.5 py-1 rounded hover:bg-gray-900 hover:text-white transition-colors duration-300"
+>
   <slot />
 </button>
