79fab31d9ad147f822f4bf7ad72e46054d378561
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-03 12:10:58 +0200

    update editor theme

--- src/lib/editorConfig.ts
+++ src/lib/editorConfig.ts
@@ -1,51 +1,16 @@
 import { javascript } from '@codemirror/lang-javascript'
-import { createTheme } from 'thememirror'
-import { tags as t } from '@lezer/highlight'
+import { customTheme } from './editorTheme'
 
-// Author: TextMate
-export const theme = createTheme({
-	variant: 'light',
-	settings: {
-		background: '#FFFFFF',
-		foreground: '#212529',
-		caret: '#212529',
-		selection: '#748ffc',
-		gutterBackground: '#FFFFFF',
-		gutterForeground: '#00000070',
-		lineHighlight: '#e7f5ff'
-	},
-	styles: [
-		{
-			tag: t.comment,
-			color: '#adb5bd'
-		},
-		{
-			tag: [t.keyword, t.operator, t.typeName, t.tagName, t.propertyName],
-			color: '#1864ab',
-			fontWeight: 'bold'
-		},
-		{
-			tag: [t.attributeName, t.definition(t.propertyName)],
-			color: '#364fc7'
-		},
-		{
-			tag: [t.className, t.string, t.special(t.brace)],
-			color: '#ff922b'
-		},
-		{
-			tag: t.number,
-			color: '#2f9e44',
-			fontWeight: 'bold'
-		},
-		{
-			tag: t.variableName,
-			fontWeight: 'bold',
-			color: '#212529'
-		}
-	]
-})
 
 export const editorConfig = {
-	lang: javascript(),
-	theme
+  basic: true,
+  useTab: true,
+  editable: true,
+  lineWrapping: false,
+  readonly: false,
+  tabSize: 2,
+  placeholder: null,
+  lang: javascript(),
+  nodebounce: false,
+  extensions: [customTheme]
 }
---
+++ src/lib/editorTheme.ts
@@ -0,0 +1,86 @@
+import { EditorView } from "@codemirror/view"
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
+import { tags as t } from "@lezer/highlight"
+import colors from "open-color"
+
+//https://github.com/codemirror/theme-one-dark/blob/main/src/one-dark.ts
+
+const theme = EditorView.theme({
+  "&": {
+    color: colors.gray[7],
+    backgroundColor: colors.white
+  },
+  "&.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection": { backgroundColor: colors.blue[1] },
+  ".cm-activeLine": { backgroundColor: colors.blue[8] + '0b' },
+}, { dark: false })
+
+const highlightStyle = HighlightStyle.define([
+  {
+    tag: t.keyword,
+    color: colors.violet[8]
+  },
+  {
+    tag: [t.name, t.deleted, t.character, t.propertyName, t.macroName],
+    color: colors.yellow[7]
+  },
+  {
+    tag: [t.function(t.variableName), t.labelName],
+    color: colors.blue[8]
+  },
+  {
+    tag: [t.color, t.constant(t.name), t.standard(t.name)],
+    color: colors.orange[7]
+  },
+  {
+    tag: [t.definition(t.name), t.separator],
+    color: colors.blue[6]
+  },
+  {
+    tag: [t.typeName, t.className, t.number, t.changed, t.annotation, t.modifier, t.self, t.namespace],
+    color: colors.indigo[7]
+  },
+  {
+    tag: [t.operator, t.operatorKeyword, t.url, t.escape, t.regexp, t.link, t.special(t.string)],
+    color: colors.lime[6]
+  },
+  {
+    tag: [t.meta, t.comment],
+    color: colors.gray[5]
+  },
+  {
+    tag: t.strong,
+    fontWeight: "bold"
+  },
+  {
+    tag: t.emphasis,
+    fontStyle: "italic"
+  },
+  {
+    tag: t.strikethrough,
+    textDecoration: "line-through"
+  },
+  {
+    tag: t.link,
+    color: colors.gray[6],
+    textDecoration: "underline"
+  },
+  {
+    tag: t.heading,
+    fontWeight: "bold",
+    color: colors.blue[9]
+  },
+  {
+    tag: [t.atom, t.bool, t.special(t.variableName)],
+    color: colors.violet[7]
+  },
+  {
+    tag: [t.processingInstruction, t.string, t.inserted],
+    color: colors.lime[6]
+  },
+  {
+    tag: t.invalid,
+    color: colors.red[9]
+  },
+])
+
+export const customTheme = [theme, syntaxHighlighting(highlightStyle)]
