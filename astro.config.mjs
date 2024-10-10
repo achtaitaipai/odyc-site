3af2eada2d637cc9ae9a0a6697db0332faec269d
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-10 20:02:27 +0200

    update astro config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import ghAlerts from "remark-gh-alerts";
-import expressiveCode from "astro-expressive-code";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import ghAlerts from 'remark-gh-alerts'
+import expressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://odyc-site.pages.dev/",
-  integrations: [svelte(), tailwind(), expressiveCode({
-    themes: 'github-dark-high-contrast'
-  })],
-  markdown: {
-    remarkPlugins: [ghAlerts]
-  }
-});
+	site: 'https://odyc.dev/',
+	integrations: [
+		svelte(),
+		tailwind(),
+		expressiveCode({
+			themes: 'github-dark-high-contrast'
+		})
+	],
+	markdown: {
+		remarkPlugins: [ghAlerts]
+	}
+})
+
