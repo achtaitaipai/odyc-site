5c7e745d60484cc5f5cc3e9bc4cb64238f21dda4
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-10 18:03:13 +0200

    update editor

--- src/lib/editorConfig.ts
+++ src/lib/editorConfig.ts
@@ -1,16 +1,16 @@
 import { javascript } from '@codemirror/lang-javascript'
 import { customTheme } from './editorTheme'
-
+import { vim } from '@replit/codemirror-vim'
 
 export const editorConfig = {
-  basic: true,
-  useTab: true,
-  editable: true,
-  lineWrapping: false,
-  readonly: false,
-  tabSize: 2,
-  placeholder: null,
-  lang: javascript(),
-  nodebounce: false,
-  extensions: [customTheme]
+	basic: true,
+	useTab: true,
+	editable: true,
+	lineWrapping: false,
+	readonly: false,
+	tabSize: 2,
+	placeholder: null,
+	lang: javascript(),
+	nodebounce: false,
+	extensions: [customTheme, ...(location.hash === '#vim' ? [vim()] : [])]
 }
